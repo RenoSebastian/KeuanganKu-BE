a0b2fda6783e23c34aebe028c5900833d1d4086f
RenoSebastian <pssttt1@gmail.com>
2026-01-25 17:37:18 +0700

    bugfix

--- src/modules/financial/financial.module.ts
+++ src/modules/financial/financial.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FinancialController } from './financial.controller';
 import { FinancialService } from './financial.service';
+import { PrismaModule } from '../../../prisma/prisma.module'; // Pastikan import Prisma jika Service butuh
 
 @Module({
+  imports: [PrismaModule], // Tambahkan jika FinancialService menggunakan Prisma
   controllers: [FinancialController],
-  providers: [FinancialService]
+  providers: [FinancialService],
+  exports: [FinancialService] // <--- WAJIB DITAMBAHKAN
 })
 export class FinancialModule {}
